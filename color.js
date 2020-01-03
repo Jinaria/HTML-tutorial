f0198d780146c5cbc6c16c6a5fc989f6ad56d037
진승기 <wlstmdrl0925@gmail.com>
2020-01-04 00:52:12 +0900

    Finish javascript class

---
+++ color.js
@@ -0,0 +1,37 @@
+var Links = {
+    setColor:function(color){
+        var alist = document.querySelectorAll('a');
+        var i = 0;
+        while(i < alist.length){
+            alist[i].style.color = color;
+            i = i + 1;
+        }
+    }
+}
+var Body = {
+    setColor:function(target, color){
+        target.style.color = color;
+    },
+    setBackgroundColor:function(target, color){
+        target.style.backgroundColor = color;
+    }
+}
+
+
+function nightDayHandler(self){
+    var target = document.querySelector('body');
+    if(self.value === 'night'){
+        Body.setBackgroundColor(target, 'black');
+        Body.setColor(target, 'white');
+        self.value='day';
+
+        Links.setColor('powderblue');
+    }
+    else{
+        Body.setBackgroundColor(target, 'white');
+        Body.setColor(target, 'black');
+        self.value='night';
+        
+        Links.setColor('blue');
+    }
+}
